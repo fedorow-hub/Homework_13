60dd886878374a4b34d7ceb8f4b9c2a49b547aa0
Sergei <s_fedorow@mail.ru>
2023-03-26 10:44:03 +0700

    Запуск и остановка хоста при запуске и остановке приложения

--- Homework_13/App.xaml.cs
+++ Homework_13/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BankDAL.DataAccess;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Windows;
 
 namespace Homework_13;
@@ -10,9 +11,26 @@ namespace Homework_13;
 /// </summary>
 public partial class App : Application
 {
-    
+    private static IHost _host;
+    public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
     public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
     {
         services.AddSingleton<DataAccess>();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        var host = Host;
+        base.OnStartup(e);
+        await host.StartAsync().ConfigureAwait(false);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        var host = Host;
+        base.OnExit(e);
+        await host.StopAsync().ConfigureAwait(false);
+        host.Dispose();
+        _host = null;
     }
 }
