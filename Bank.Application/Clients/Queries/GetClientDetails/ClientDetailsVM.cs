1da6303db3cf33609283cb176a821ab61197084a
Sergei <s_fedorow@mail.ru>
2023-04-14 21:10:09 +0700

    Создана модель ClientDetailsVM

---
+++ Bank.Application/Clients/Queries/GetClientDetails/ClientDetailsVM.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Bank.Application.Common.Mapping;
+using Bank.Domain.Account;
+using Bank.Domain.Client;
+
+namespace Bank.Application.Clients.Queries.GetClientDetails;
+
+public class ClientDetailsVM : IMapWith<Client>
+{
+    public string Firstname { get; }
+
+    public string Lastname { get; set; }
+
+    public string Patronymic { get; }
+
+    public string PhoneNumber { get; set; }
+
+    public string PassportSerie { get; set; }
+
+    public string PassportNumber { get; set; }
+
+    public List<PlainAccount> plainAccounts { get; set; } = new List<PlainAccount>();
+
+    public List<DepositAccount> depositAccounts { get; set; } = new List<DepositAccount>();
+
+    public List<CreditAccount> creditAccounts { get; set; } = new List<CreditAccount>();
+
+public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Client, ClientDetailsVM>()
+            .ForMember(clientVm => clientVm.Firstname,
+                opt => opt.MapFrom(client => client.Firstname))
+            .ForMember(clientVm => clientVm.Lastname,
+                opt => opt.MapFrom(client => client.Lastname))
+            .ForMember(clientVm => clientVm.Patronymic,
+                opt => opt.MapFrom(client => client.Patronymic))
+            .ForMember(clientVm => clientVm.PhoneNumber,
+                opt => opt.MapFrom(client => client.PhoneNumber.Number))
+            .ForMember(clientVm => clientVm.PassportSerie,
+                opt => opt.MapFrom(client => client.PassportSerie.Serie))
+            .ForMember(clientVm => clientVm.PassportNumber,
+                opt => opt.MapFrom(client => client.PassportNumber.Number));
+    }
+}
